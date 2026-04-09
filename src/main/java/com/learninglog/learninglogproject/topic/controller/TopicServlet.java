e0da2ac0cbd7ecfbdd462d582a589f1cc92f6b4e
Manilrai <raeemanil123@gmail.com>
2026-04-09 11:45:30 +0545

    Project upload

---
+++ src/main/java/com/learninglog/learninglogproject/topic/controller/TopicServlet.java
@@ -0,0 +1,68 @@
+package com.learninglog.learninglogproject.topic.controller;
+
+import com.learninglog.learninglogproject.topic.model.Topic;
+import com.learninglog.learninglogproject.topic.model.dao.TopicDao;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+
+@WebServlet("/topic")
+public class TopicServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+
+//        if(action.equals("list")){
+        if("list".equals(action)){
+            try {
+                List<Topic> topicList = TopicDao.fetchTopics();
+                req.setAttribute("topics", topicList);
+            } catch (Exception e) {
+                req.setAttribute("error", "Unable to fetch");
+            }
+            req.getRequestDispatcher("pages/topic-list.jsp").forward(req,resp);
+        }
+        req.getRequestDispatcher("pages/add-topic.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse rest){
+        String  action = req.getParameter("action");
+
+        if(action=="add"){
+            String topicName = req.getParameter("topic-name");
+            int userId = Integer.parseInt(req.getParameter("userId"));
+            Timestamp createdAt = new Timestamp(System.currentTimeMillis());
+            Timestamp updatedDate = createdAt;
+            Topic topicObj = new Topic();
+            topicObj.setname(topicName);
+            topicObj.setUserId(userId);
+            topicObj.setCreatedAt(createdAt);
+            topicObj.setUpdatedAt(updatedDate);
+
+            try{
+                boolean result = TopicDao.insertTopic(topicObj);
+                if(result){
+                    req.setAttribute("success", "Successfully topic added");
+                }
+                else {
+                    req.setAttribute("error","Something went wrong");
+                }
+                req.getRequestDispatcher("pages/add-topic.jsp").forward(req,rest);
+            }
+            catch (Exception e){
+            }
+
+            // Call the topic dao
+
+
+        }
+    }
+}
